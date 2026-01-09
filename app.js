fc5783f48ed7d7ebe7d17b76854e5b7dba2d4856
Fabricio Dev <fabriciodeveloper1997@gmail.com>
2026-01-08 23:47:29 -0300

    novo commit0801

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const switcher = document.querySelector('.btn');
+const terror = document.querySelector('.img');
+const isDark = document.body.classList.contains('dark-theme');
 
         switcher.addEventListener('click', function() {
         document.body.classList.toggle("dark-theme")
@@ -11,6 +13,11 @@ const switcher = document.querySelector('.btn');
         }
         else{
             this.textContent = "Light";
+            getelementby
         }
         console.log('current class name: '+ className);
+
+        /* Fazer algo para monstrar a imagem apenas quando estiver em Dark theme, quando estiver no light não mostrar a imagem
+        uma espécie de susto para o usuário */
+    
     });
